686f4157e8fb9fb68763b667efbcc56cf5f3cdaa
Abhishek Pandey <33335446+EnRyuuX7@users.noreply.github.com>
2022-06-18 22:17:49 +0545

    fixed about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -20,7 +20,7 @@ const About = () => (
                         </div>
 
                         <div className='two-thirds column'>
-                            <p className={styles.lead}>Born in Kathmandu Nepal, I am a UI/UX designer as well as a frontend Developer with a passion for designing and developing different web-based applications.
+                            <p className={styles.lead}>Born in Kathmandu Nepal, I am a ReactJS developer as well as a UI/UX designer with a passion for designing and developing different web-based applications.
                                 <br></br><br></br>I have a trait for many things such as problem-solving, app-development and learning new and upcoming technologies.
                                 <br></br><br></br>Besides working I love to play the piano, play games, draw, read manga, watch anime and listen to music. <br></br><br></br>Currently, I am in my final year in BSc.CSIT at Deerwalk Institute of Technology.
                             </p>
