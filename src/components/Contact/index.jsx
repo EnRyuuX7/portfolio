69618a9748dbcfb4b024647a5feebdea0926ecd1
Abhishek Pandey <33335446+EnRyuuX7@users.noreply.github.com>
2022-07-08 14:27:20 +0545

    temp Push

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -31,64 +31,78 @@ const ContactCards = ({ icon, type, value }) => (
 );
 
 
-const Contact = () => (
-    <section id="contact" className={styles.contact}>
+const Contact = () => {
+    const { submit } = useWeb3forms({
+        apikey: "22b1ad2a-7d14-4c33-9ea0-9e5859f4309a",
+        onSuccess: (successMessage, data) => {
+            console.log(successMessage, data)
+        },
+        onError: (errorMessage, data) => {
+            console.log(errorMessage, data)
+        },
+    });
+    return (
+        <section id="contact" className={styles.contact}>
 
-        <div className="section__heading page__heading column" >
-            <h2 className="section__title">Get in Touch</h2>
-            <div className="animated__bar"></div>
-        </div>
-        <div className={styles.contact__section}>
-            <div className="row">
-                <div className="two-thirds column">
-                    {/* <form className={styles.contact__form} id={styles.contact__form}
+            <div className="section__heading page__heading column" >
+                <h2 className="section__title">Get in Touch</h2>
+                <div className="animated__bar"></div>
+            </div>
+            <div className={styles.contact__section}>
+                <div className="row">
+                    <div className="two-thirds column">
+                        {/* <form className={styles.contact__form} id={styles.contact__form}
                         action="https://api.web3forms.com/submit" method="POST">
                         <input type="hidden" name="access_key" value="22b1ad2a-7d14-4c33-9ea0-9e5859f4309a" />
                         <input type="hidden" name="subject" value="New Message from Portfolio" />
                         <input type="checkbox" name="botcheck" id="" /> */}
-                    <form className={styles.contact__form} id={styles.contact__form}
-                        action="#">
+                        <form className={styles.contact__form} id={styles.contact__form}
+                            action="#">
 
-                        <h4 className={styles.content__title}>Message Me</h4>
-                        <div className="row">
-                            <div className={styles.group}>
-                                <div className="one-half column">
-                                    <div className={`${styles.form__group} ${styles.gap}`}><input className={styles.form__control} id={styles.contact__name} type="text"
-                                        name="name" placeholder="Name" required="" /></div>
+                            <h4 className={styles.content__title}>Message Me</h4>
+                            <div className="row">
+                                <div className={styles.group}>
+                                    <div className="one-half column">
+                                        <div className={`${styles.form__group} ${styles.gap}`}><input className={styles.form__control} id={styles.contact__name} type="text"
+                                            name="name" placeholder="Name" required="" /></div>
+                                    </div>
+                                    <div className="one-half column">
+                                        <div className={`${styles.form__group} ${styles.gap} ${styles.c__email}`}><input className={styles.form__control} id={styles.contact__email}
+                                            type="email" name="email" placeholder="Email" required="" /></div>
+                                    </div>
                                 </div>
-                                <div className="one-half column">
-                                    <div className={`${styles.form__group} ${styles.gap} ${styles.c__email}`}><input className={styles.form__control} id={styles.contact__email}
-                                        type="email" name="email" placeholder="Email" required="" /></div>
+                                <div className={`column ${styles.form__group} nomargin`}><input className={styles.form__control} id={styles.contact__subject} type="text"
+                                    name="subject" placeholder="Subject" autoComplete="off" required="" /></div>
+                                <div className={`column ${styles.form__group} nomargin`}><textarea className={styles.form__control} id={styles.contact__message}
+                                    name="message" placeholder="Message" rows="5" required=""></textarea></div>
+                                <div className={`column ${styles.form__submit} nomargin`}><button className={`${styles.btn}`} id={styles.contact__submit}
+                                    data-tip="Not Implemented yet Sorry!!"
+                                    onClick={(e) => {
+                                        submit({
+                                            hello: "world",
+                                            isWorking: "Yesss!!!",
+                                        });
+                                    }} >Send Message</button>
+                                    <p className={styles.contact__feedback}></p>
                                 </div>
                             </div>
-                            <div className={`column ${styles.form__group} nomargin`}><input className={styles.form__control} id={styles.contact__subject} type="text"
-                                name="subject" placeholder="Subject" autoComplete="off" required="" /></div>
-                            <div className={`column ${styles.form__group} nomargin`}><textarea className={styles.form__control} id={styles.contact__message}
-                                name="message" placeholder="Message" rows="5" required=""></textarea></div>
-                            <div className={`column ${styles.form__submit} nomargin`}><button className={`${styles.btn}`} id={styles.contact__submit}
-                                data-tip="Not Implemented yet Sorry!!" >Send Message</button>
-                                <p className={styles.contact__feedback}></p>
-                            </div>
+                        </form>
+                    </div>
+                    <div className="one-third column">
+                        <div className={styles.contact__info}>
+                            <h4 className={styles.content__title}>Contact Info</h4>
+                            <ul className={`${styles.list__unstyled} ${styles.list__info}`}>
+                                {ContactInfo.map((info, index) => (
+                                    <ContactCards key={index} icon={info.icon} type={info.type} value={info.value} />
+                                ))}
+                            </ul>
                         </div>
-                    </form>
-                </div>
-                <div className="one-third column">
-                    <div className={styles.contact__info}>
-                        <h4 className={styles.content__title}>Contact Info</h4>
-                        <ul className={`${styles.list__unstyled} ${styles.list__info}`}>
-                            {ContactInfo.map((info, index) => (
-                                <ContactCards key={index} icon={info.icon} type={info.type} value={info.value} />
-                            ))}
-                        </ul>
                     </div>
                 </div>
             </div>
-        </div>
-        <ReactTooltip place="right" type="light" effect="solid" />
-    </section >
+            <ReactTooltip place="right" type="light" effect="solid" />
+        </section >
 
-
-
-);
-
+    )
+}
 export default Contact;
